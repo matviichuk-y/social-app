15630c323b4ca0d646bffd4e7a0eb1735c580117
Yevhenii <matviichukeugene2101@gmail.com>
2024-06-05 18:23:35 +0300

    Created first Auth Page

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const SignUpValidation = z.object({
+  name: z.string().min(2, { message: "Too short name, must be at least 2 characters" }),
+  username: z.string().min(2, { message: "Too short username, must be at least 2 characters" }),
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Too short password, must be at least 8 characters" }),
+});
